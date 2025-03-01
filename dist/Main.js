9bff972cefaae4f2bbd9e4b74444f26e741fe43d
Evahn <evahn95@gmail.com>
2025-03-01 14:18:24 -0500

    dfsa

--- dist/Main.js
+++ dist/Main.js
@@ -47,7 +47,7 @@ goToRegisterBtn.style.color = "white";
 
 // Create the login image
 const loginImage = document.createElement("img");
-loginImage.src = "dist/Screenshot 2025-03-01 at 1.44.07 PM.png"; // Replace with actual URL or file path
+loginImage.src = "Screenshot 2025-03-01 at 1.44.07 PM.png"; // Replace with actual URL or file path
 loginImage.alt = "Login Image";
 loginImage.style.width = "100%"; // Adjust width as needed
 loginImage.style.maxWidth = "150px"; // Controls max size
